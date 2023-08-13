f17eccb9ffae4acbe32a58320b2deea4ef2ef42e
Erenyesiltepe <eren.bilkentk15@hotmail.com>
2023-08-13 14:50:04 +0300

    new scrapers added

---
+++ tmp.py
@@ -0,0 +1,31 @@
+for event in events:
+    date=event.span.text
+    if date!="":
+        date=getTime(date)
+
+    alink=event.a["href"]
+    category=
+    
+    print(alink)
+
+
+def getTime(time_str):
+    months={
+        "Ocak":1,
+        "Şubat":2,
+        "Mart":3,
+        "Nisan":4,
+        "Mayıs":5,
+        "Haziran":6,
+        "Temmuz":7,
+        "Ağustos":8,
+        "Eylül":9,
+        "Ekim":10,
+        "Kasım":11,
+        "Aralık":12,
+    }
+    time=time_str.split(" ")
+    day=int(time[0])
+    month=months[time[1]]
+    year=int(time[2])
+    return datetime(year,month,day)
